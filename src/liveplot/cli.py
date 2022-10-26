cd0056266895efca74baaf1272716bf1ef37f9fd
Frederik <frederik.kunstner@gmail.com>
2022-10-25 20:00:04 -0700

    cleanup cli

--- src/liveplot/cli.py
+++ src/liveplot/cli.py
@@ -4,25 +4,30 @@ import logging
 import sys
 import textwrap
 from pathlib import Path
+from typing import Optional
 
 from liveplot.plot_watcher import PlotWatcher
 
+default_new_template_file = "new_liveplot.py"
 
-def create_template(filepath: Path):
+
+def create_template(filepath: Optional[Path]) -> Path:
+    if filepath is None:
+        filepath = Path(default_new_template_file)
+
     if filepath.exists():
-        print(f"File {filepath} already exists")
-    else:
-        with importlib.resources.path("liveplot", "template.py") as template:
-            with open(filepath, "w", encoding="utf8") as f:
-                f.write(template.read_text(encoding="utf8"))
+        raise FileExistsError(f"File {filepath} already exists")
+
+    with importlib.resources.path("liveplot", "template.py") as template:
+        with open(filepath, "w", encoding="utf8") as f:
+            f.write(template.read_text(encoding="utf8"))
+
+    return filepath
 
 
 def make_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        description=(
-            "Update a Matplotlib pyplot figure on save, "
-            "without having to re-run a script or reload data"
-        ),
+        description="Update a Matplotlib pyplot figure on save",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent(
             """
@@ -52,8 +57,14 @@ def make_parser() -> argparse.ArgumentParser:
             Find the documentation and examples at https://github.com/fkunstner/liveplot
             """
         ),
-        usage="""liveplot [-h] (script | --new [filename]) [--debug]
-        """,
+    )
+
+    parser.add_argument(
+        "--new",
+        "-n",
+        action="store_true",
+        default=False,
+        help=f"Create a plotting script in script (default: {default_new_template_file})",
     )
     parser.add_argument(
         "script",
@@ -63,20 +74,10 @@ def make_parser() -> argparse.ArgumentParser:
         help="The plotting file to update on change",
     )
     parser.add_argument(
-        "--new",
-        metavar="filename",
-        action="store",
-        nargs="?",
-        const=Path("new_plot.py"),
-        default=None,
-        type=Path,
-        help=(
-            "Create a plotting script template "
-            "in filename (defaults to new_liveplot.py)"
-        ),
-    )
-    parser.add_argument(
-        "--debug", action="store_true", default=False, help="Enable debug logs"
+        "--debug",
+        action="store_true",
+        default=False,
+        help="Enable debug logs",
     )
     return parser
 
@@ -95,12 +96,7 @@ def configure_logs(debug: bool = False):
     logger.addHandler(handler)
 
 
-def launch_liveplot(filepath: Path, debug: bool):
-    if not filepath.is_file():
-        raise FileNotFoundError(f"{filepath}")
-
-    configure_logs(debug)
-
+def launch_liveplot(filepath: Path):
     figure_watcher = PlotWatcher.from_path(filepath)
     while True:
         figure_watcher.refresh()
@@ -111,13 +107,15 @@ def main():
     parser = make_parser()
     cli_args = parser.parse_args()
 
-    no_input = cli_args.script is None and cli_args.new is None
+    configure_logs(cli_args.debug)
 
-    if no_input:
+    no_args = cli_args.new is False and cli_args.script is None
+
+    if no_args:
         parser.print_help()
-    elif cli_args.new is not None:
-        create_template(cli_args.new)
-    elif cli_args.script is not None:
-        launch_liveplot(cli_args.script, cli_args.debug)
+        sys.exit()
 
-    sys.exit()
+    if cli_args.new is True:
+        cli_args.script = create_template(cli_args.script)
+
+    launch_liveplot(cli_args.script)
